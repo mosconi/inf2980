3fccd3c7f6c4b60ad609f81617165ed26a2ea7a4
Rodrigo Mosconi <mosconi.rmg@gmail.com>
2016-05-15 11:26:24 -0300

    priorizando algumas instancias

---
+++ trab1/run2_allN.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd datasets
+for i in N*.bz2 ; do
+    echo -n "$i"
+    for j in 0 1 2 3 4 5 6 7 8 9; do
+       echo -n " $j"
+       [ -f ../results/${i}.output_2_${j} ] && continue
+       python2.7 ../run2.py ${i} > ${i}.output_2_${j}
+       mv ${i}.output_2_${j} ../results/
+    done
+    echo " done"
+done
